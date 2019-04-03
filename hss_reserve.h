d4a9c9a6559659f098acd9be0871cf146e3bbf5d
sfluhrer <sfluhrer@cisco.com>
2019-04-03 17:52:35 -0400

    Fault detection changes

--- hss_reserve.h
+++ hss_reserve.h
@@ -13,9 +13,6 @@ struct hss_working_key;
 void hss_set_reserve_count(struct hss_working_key *w, sequence_t count);
 
 bool hss_advance_count(struct hss_working_key *w, sequence_t new_count,
-        bool (*update_private_key)(unsigned char *private_key,
-                size_t len_private_key, void *context),
-        void *context,
         struct hss_extra_info *info, bool *trash_private_key);
 
 #endif /* HSS_RESERVE_H_ */
