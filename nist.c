dcb2945e252ffb03a73ba2a72b6eff3d773991bc
Duc Nguyen <ductri.nguyen@sandboxquantum.com>
2022-06-06 16:10:28 -0400

    add keysize in constant-time style

--- nist.c
+++ nist.c
@@ -10,12 +10,14 @@
 *
 * Arguments:   - uint8_t *pk: pointer to output public key (allocated
 *                             array of LMS_CRYPTO_PUBLICKEYBYTES bytes)
+*              - size_t *pklen: pointer to public key length (0 if failed)
 *              - uint8_t *sk: pointer to output private key (allocated
 *                             array of LMS_CRYPTO_SECRETKEYBYTES bytes)
+*              - size_t *sklen: pointer to private key length (0 if failed)
 *
 * Returns 0 (success)
 **************************************************/
-int crypto_sign_keypair(uint8_t *pk, uint8_t *sk)
+int crypto_sign_keypair(uint8_t *pk, size_t *pklen, uint8_t *sk, size_t *sklen)
 {
     uint8_t buf[48];
     /* Select NIST KAT as random generator*/
@@ -28,13 +30,8 @@ int crypto_sign_keypair(uint8_t *pk, uint8_t *sk)
     OQS_randombytes(buf, 48);
     OQS_randombytes_nist_kat_init_256bit(buf, NULL);
 
-#if NIST_LEVEL == 1
-    param_set_t lm_type[]  = {PARAM_LM_HEIGHT};
-    param_set_t ots_type[] = {PARAM_OTS_WIDTH};
-#else 
     param_set_t lm_type[]  = {PARAM_LM_HEIGHT0, PARAM_LM_HEIGHT1};
     param_set_t ots_type[] = {PARAM_OTS_WIDTH , PARAM_OTS_WIDTH};
-#endif
     unsigned levels = PARAM_LEVEL;
 
     /* Generate keypair using LMS API */
@@ -46,19 +43,22 @@ int crypto_sign_keypair(uint8_t *pk, uint8_t *sk)
     
     int pubkey_size = hss_get_public_key_len(levels, lm_type, ots_type);
     int privkey_size = hss_get_private_key_len(levels, lm_type, ots_type);
-    
-    hss_generate_private_key(OQS_randombytes, levels, lm_type, ots_type,
+    bool ret = hss_generate_private_key(OQS_randombytes, levels, lm_type, ots_type,
                              NULL, sk, pk, pubkey_size, aux_data, aux_data_len, NULL);
+
+    /* Branchless if-else magic */
+    *pklen = (-ret) & (pubkey_size);
+    *sklen = (-ret) & (privkey_size);
 
     free(aux_data);
 
     return OQS_SUCCESS;
 }
 
-
 int crypto_sign(uint8_t *sig, size_t *siglen,
                 const uint8_t *m, size_t mlen, const uint8_t *sk)
 {
+    
 }
 
 int crypto_sign_open(uint8_t *m, size_t *mlen,
